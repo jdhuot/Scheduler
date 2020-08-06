27093654e509c88bf03555ac0c55c8235ff847b0
Jeremy Huot <logflume@live.com>
2020-08-05 18:02:06 -0600

    Remove old hard coded Days array

--- src/components/Application.js
+++ src/components/Application.js
@@ -5,23 +5,6 @@ import axios from 'axios';
 
 import "components/Application.scss";
 
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
 
 
 const appointments = [
