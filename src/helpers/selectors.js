2dfe264d5dd10855f5f5a44813cb090f5427bbc5
Jeremy Huot <logflume@live.com>
2020-08-05 18:53:36 -0600

    Add selector function to format appointments data from days

---
+++ src/helpers/selectors.js
@@ -0,0 +1,13 @@
+
+
+
+export function getAppointmentsForDay(state, day) {
+  let appts = [];
+  for (let i of state.days)
+  if (i.name === day) {
+    for (let j of i.appointments) {
+      appts.push(state.appointments[j])
+    }
+  }
+  return appts;
+}
